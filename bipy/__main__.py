5a92620207d9b4ef5d46f40724647e7eb569d584
Tai Danh <taid100@gmail.com>
2021-06-02 12:42:00 -0700

    close input file & can read stdin

--- bipy/__main__.py
+++ bipy/__main__.py
@@ -11,23 +11,25 @@ from tape import Tape
 
 
 def main() -> None:
-    program = None
+    program = sys.stdin
 
     # CLI Arguments
-    if len(sys.argv) != 2:
+    if len(sys.argv) > 2:
         util.printerr("Usage:")
         util.printerr("    {} <input file>".format(sys.argv[0]))
         exit(2)
 
-    try:
-        program = open(sys.argv[1], "r")
-    except OSError:
-        util.printerr("Failed to open '{}'".format(sys.argv[1]))
-        exit(2)
+    if len(sys.argv) == 2:
+        try:
+            program = open(sys.argv[1], "r")
+        except OSError:
+            util.printerr("Failed to open '{}'".format(sys.argv[1]))
+            exit(2)
 
     tape = Tape(30000)
     code = "".join(program.readlines())
     bipy.evaluate(code, tape)
+    program.close()
 
 
 if __name__ == "__main__":
